1775cc5d78f7e2c3e802e06989f2a393e386f303
León Cantillo <leonjob13@gmail.com>
2023-12-28 16:06:53 -0500

    async & await

---
+++ src/async/index.js
@@ -0,0 +1,20 @@
+// Promesa
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Async!!'), 2000)
+            : reject(new Error('Error!'))
+    })
+}
+
+// Async & Await
+const anotherFn = async () => {
+    const something = await fnAsync()
+    console.log(something)
+    console.log('Chanchito Feliz!')
+}
+
+// Ejecution
+console.log('Before')
+anotherFn()
+console.log('After')
